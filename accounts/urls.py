3dca87ea61e565ef18a1daf51e6265e341328e47
Alagani Jagadeesh <144417797+Alagani@users.noreply.github.com>
2024-01-23 00:35:46 +0530

    Update urls.py

--- accounts/urls.py
+++ accounts/urls.py
@@ -7,3 +7,5 @@ urlpatterns=[
     path('logout/',logout_user,name='logout')
 ]
 
+urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
